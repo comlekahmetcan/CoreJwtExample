7d7716fc84dce389745d17b553e66337d26f4335
AhmetCan ÇÖMLEK <ahmetcan.comlek@cts.com.tr>
2022-11-09 15:38:06 +0300

    Studentcontroller oluşturuldu

---
+++ CoreJwtExample/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using CoreJwtExample.IRepository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace CoreJwtExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private IConfiguration _config;
+        IStudentRepository _oStudentRepository = null;
+
+        public StudentController(IConfiguration config, IStudentRepository oStudentRepository)
+        {
+            _config = config;
+            _oStudentRepository = oStudentRepository;
+        }
+
+        [HttpGet]
+        [Route("Gets1")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> Gets1()
+        {
+            var list = await _oStudentRepository.Gets1();
+            return Ok(list);
+        }
+        [HttpGet]
+        [Route("Gets2")]
+        public async Task<IActionResult> Gets2()
+        {
+            var list = await _oStudentRepository.Gets2();
+            return Ok(list);
+        }
+    }
+}
